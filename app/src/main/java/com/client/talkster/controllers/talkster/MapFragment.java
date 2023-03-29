d3f094102f63cf920d6fd7e92fe2404fe1fb4e5e
Levente Szénási <senasilevixd@gmail.com>
2023-03-29 16:16:27 +0200

    Styled google maps

--- app/src/main/java/com/client/talkster/controllers/talkster/MapFragment.java
+++ app/src/main/java/com/client/talkster/controllers/talkster/MapFragment.java
@@ -1,43 +1,40 @@
 package com.client.talkster.controllers.talkster;
 
-import android.os.Build;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
 import com.client.talkster.R;
-import com.client.talkster.api.APIStompWebSocket;
 import com.client.talkster.classes.UserJWT;
-import com.client.talkster.dto.MessageDTO;
 import com.client.talkster.interfaces.IFragmentActivity;
-import com.client.talkster.utils.enums.MessageType;
-import com.google.gson.Gson;
 
-import java.time.OffsetDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import ua.naiksoftware.stomp.StompHeader;
-import ua.naiksoftware.stomp.client.StompClient;
-
-public class MapFragment extends Fragment implements IFragmentActivity
+public class MapFragment extends Fragment implements IFragmentActivity, OnMapReadyCallback
 {
     private UserJWT userJWT;
+    private MapView mapView;
 
     public MapFragment(UserJWT userJWT)
     {
         this.userJWT = userJWT;
     }
-
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+    private Bundle mapViewBundle = null;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
     }
 
@@ -46,13 +43,79 @@ public class MapFragment extends Fragment implements IFragmentActivity
     {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
         getUIElements(view);
-
+        initGooglemaps(savedInstanceState);
         return view;
+    }
+
+    private void initGooglemaps(Bundle savedInstanceState){
+        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
+        // objects or sub-Bundles.
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+
+        mapView.onCreate(mapViewBundle);
+
+        mapView.getMapAsync(this);
     }
 
     @Override
     public void getUIElements(View view)
     {
+        mapView = (MapView) view.findViewById(R.id.mapView);
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = map.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            getContext(), R.raw.style_light));
+
+            if (!success) {
+                Log.e("Google Maps", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("Google Maps", "Can't find style. Error: ", e);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
